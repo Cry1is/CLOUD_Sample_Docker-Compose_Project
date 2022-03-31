d208bf25ed5d14b89407c3e47c1ec2da84cb8960
wesoa012 <wesa@smu.edu>
2022-03-30 19:49:27 -0500

    Sign Up Page is now a React Component

--- frontend/src/Components/Login/SignUpPage.jsx
+++ frontend/src/Components/Login/SignUpPage.jsx
@@ -1,73 +1,90 @@
 import { PasswordField, SelectField, TextField } from "../common";
-import { useState } from "react";
-import { GenericButton } from "../common/GenericButton";
-import { addAccount } from "../APIFolder/loginApi";
+// import { useState } from "react";
+// import { GenericButton } from "../common/GenericButton";
+import { addAccount } from "../../APIFolder/loginApi";
 import { useNavigate } from "react-router-dom";
 import Cookies from "js-cookie";
+import React from "react";
 
-export const SignUpPage = () => {
-    const navigate = useNavigate();
-    const login = (response) => {
-        if(response.success === 1){
-        Cookies.set("username", `${username}`);
-        navigate('/');
-    }else{
-        window.alert("Failed to Sign Up. Username is taken");
-    }}
-    const [firstName, setFirstName] = useState('');
-    const [lastName, setLastName] = useState('');
-    const [email, setEmail] = useState('');
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
-    const [ accountType, setAccountType ] = useState('');
-    const [ accountTypes ] = useState([
-        "Student",
-        "Teacher"
-    ]);
 
-    const [ account, setAccount ] = useState([]);
+export default class SignUpPage extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+        
+        this.state.firstName = '';
+        this.state.lastName = '';
+        this.state.email = '';
+        this.state.username = '';
+        this.state.password = '';
+        this.state.accountType = '';
+        this.state.accountTypes = [
+            "Student",
+            "Teacher"
+        ];
+    }
 
-    // const mergeAccount = delta => setAccount({ ...account, ...delta });
+    componentDidMount() {
 
-    return <section id="loginView">
-        <h1>Sign Up</h1>
-        <form className="container">
+    }
 
-            <TextField label="First Name"
-                value={firstName}
-                setValue={x => setFirstName(x)} />
-            <TextField label="Last Name"
-                value={lastName}
-                setValue={x => setLastName(x)} />
-            <TextField label="Email"
-                value={email}
-                setValue={x => setEmail(x)} />
+    handleChange(e) {
+        // handle state changes
+        this.setState({ value: e.target.value });
+    }
+    
+    login(response){
+        
+        if (response.success === 1) {
+            Cookies.set("username", `${this.state.username}`);
+            // useNavigate('/');
+        } else {
+            window.alert("Failed to Sign Up. Username is taken");
+        }
+    }
+
+
+    render() {
+        return (<section id="loginView">
+            <h1>Sign Up</h1>
+            <form className="container">
+
+                <TextField label="First Name"
+                    value={this.state.firstName}
+                    setValue={x => this.handleChange(x)} />
+                <TextField label="Last Name"
+                    value={this.state.lastName}
+                    setValue={x => this.handleChange(x)} />
+                <TextField label="Email"
+                    value={this.state.email}
+                    setValue={x => this.handleChange(x)} />
                 {/* May want to use for confirmations in the future? If we can figure it out */}
-            <TextField label="Username"
-                value={username}
-                setValue={x => setUsername(x)} />
+                <TextField label="Username"
+                    value={this.state.username}
+                    setValue={x => this.handleChange(x)} />
 
-            {/* Need to change all the generic classes to id passed in since
+                {/* Need to change all the generic classes to id passed in since
              I click on "Password" and it takes me to the Username field" */}
 
-            <PasswordField label="Password"
-                value={password}
-                setValue={x => setPassword(x)} />
-            <SelectField label="Account Type"
-                options={accountTypes}
-                value={accountType}
-                setValue={setAccountType} />
-            <button 
-            type="button" 
-            onClick={() => addAccount({"username": username, "password": password, "firstName": firstName, "lastName": lastName, "email": email})
-            .then(response => login(response))
-            .catch()}
-            variant="contained" 
-            color="success">
-                Sign Up
-            </button>
-            {/* <GenericButton label="Sign Up" click="/login" /> */}
-        </form>
+                <PasswordField label="Password"
+                    value={this.state.password}
+                    setValue={x => this.handleChange(x)} />
+                <SelectField label="Account Type"
+                    options={this.state.accountTypes}
+                    value={this.state.accountType}
+                    setValue={this.state.setAccountType} />
+                <button
+                    type="button"
+                    onClick={() => addAccount({ "username": this.state.username, "password": this.state.password, "firstName": this.state.firstName, "lastName": this.state.lastName, "email": this.state.email })
+                        .then(response => this.login(response))}
+                    variant="contained"
+                    color="success">
+                    Sign Up
+                </button>
+                {/* <GenericButton label="Sign Up" click="/login" /> */}
+            </form>
 
-    </section>;
+        </section>);
+    }
+
 }
